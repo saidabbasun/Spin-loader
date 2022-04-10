aef94b333e63dc0b3662770b076980c32be63290
saidabbasun <saidabboscoder0000@gmail.com>
2022-04-10 09:44:15 +0500

    Loader

---
+++ src/Loading.js
@@ -0,0 +1,57 @@
+import styled, { keyframes } from "styled-components";
+
+ const Load = keyframes`
+0% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  5%,
+  95% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+  }
+  10%,
+  59% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em, -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em, -0.297em -0.775em 0 -0.477em;
+  }
+  20% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em, -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em, -0.749em -0.34em 0 -0.477em;
+  }
+  38% {
+    box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em, -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em, -0.82em -0.09em 0 -0.477em;
+  }
+  100% {
+    box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em, 0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+}
+`;
+const round =keyframes`
+  0% {
+       
+    transform: rotate(0deg);
+  }
+  100% {
+   
+    transform: rotate(360deg);
+  }
+`;
+const LoadingDiv = styled.div`
+  animation: ${Load} 2.5s infinite ease, ${round} 2.5s infinite ease;
+  border-radius: 50%;
+  color: #ECB829;
+  font-size: 80px;
+  text-indent: -9999em;
+  overflow: hidden;
+  width: 1em;
+  height: 1em;
+  margin: 35px auto;
+  position: relative;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+ 
+`;
+
+
+
+const SpinLoader=()=>{
+   return <LoadingDiv/>
+}
+export default SpinLoader;
